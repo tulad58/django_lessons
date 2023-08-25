789e32ffebcabda7dccf48b1baf1af5acb5ccb7e
Tulad95 <link7800@yandex.ru>
2023-08-25 07:23:39 +0600

    pagination project commits

--- 1.2-requests-templates/pagination/pagination/settings.py
+++ 1.2-requests-templates/pagination/pagination/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets'),
 ]
 
-BUS_STATION_CSV = os.path.join(BASE_DIR, 'data-398-2018-08-30.csv')
+BUS_STATION_CSV = os.path.join(BASE_DIR, 'data.csv')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
-
+from django.core.paginator import Paginator
+import csv
 
 def index(request):
     return redirect(reverse('bus_stations'))
 
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
-
+    page_number = int(request.GET.get('page', 1))
+    paginator = Paginator(csv_data, per_page=10)
+    page = paginator.get_page(page_number)
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': page,
+        'page': page
     }
     return render(request, 'stations/index.html', context)
+
+
+def csv_reader(csv_file):
+    with open(csv_file, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        return list(reader)
+
+csv_data = csv_reader('data.csv')
