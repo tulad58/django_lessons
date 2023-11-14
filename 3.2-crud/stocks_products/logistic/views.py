46e9c3218a15dceef46b682891e160e3c1a748e5
Tulad95 <link7800@yandex.ru>
2023-11-14 15:44:16 +0600

    test1

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -5,6 +5,8 @@ from logistic.serializers import ProductSerializer, StockSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
 class ProductViewSet(ModelViewSet):
@@ -22,3 +24,7 @@ class StockViewSet(ModelViewSet):
     # при необходимости добавьте параметры фильтрации
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['products']
+
+@api_view(['GET'])
+def sample_view(request):
+    return Response('All works')
