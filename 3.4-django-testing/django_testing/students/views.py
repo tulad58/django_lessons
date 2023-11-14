171a09d7a67543d5516526c6b1d5b28e5c5fa8b9
Tulad95 <link7800@yandex.ru>
2023-11-14 09:09:44 +0600

    new

--- 3.4-django-testing/django_testing/students/views.py
+++ 3.4-django-testing/django_testing/students/views.py
@@ -4,6 +4,7 @@ from rest_framework.viewsets import ModelViewSet
 from students.filters import CourseFilter
 from students.models import Course
 from students.serializers import CourseSerializer
+from rest_framework.decorators import api_view
 
 
 class CoursesViewSet(ModelViewSet):
@@ -12,3 +13,8 @@ class CoursesViewSet(ModelViewSet):
     serializer_class = CourseSerializer
     filter_backends = (DjangoFilterBackend, )
     filterset_class = CourseFilter
+
+
+@api_view(['GET'])
+def ping_view(request):
+    return Response({"status": "Ok"})
--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,2 +1,60 @@
-def test_example():
-    assert False, "Just test example"
+import pytest
+from rest_framework.test import APIClient
+from model_bakery import baker
+from students.models import Student, Course
+from students.views import ping_view
+from django.urls import reverse
+from rest_framework import status
+
+
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+@pytest.fixture
+def courses_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Course, *args, **kwargs)
+    return factory
+
+
+@pytest.fixture
+def students_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Student, *args, **kwargs)
+    return factory
+
+@pytest.mark.django_db
+def test_course(client, courses_factory):
+    courses = courses_factory()
+    url = reverse("courses-list")
+    resp = client.get(url)
+    resp_json =  resp.json()
+    assert resp_json[0]['name'] == courses.name
+
+
+@pytest.mark.django_db
+def test_courses(client, courses_factory):
+    courses = courses_factory(_quantity=3)
+    url = reverse("courses-list")
+    resp = client.get(url)
+    resp_json =  resp.json()
+    for i, c in enumerate(resp_json):
+        assert c['name'] == courses[i].name
+
+
+@pytest.mark.django_db
+def test_filter_id(client, courses_factory):
+    courses = courses_factory(_quantity=3)  
+    url = reverse("courses-list")
+    resp = client.get(url)
+    resp_json =  resp.json()
+    res = resp_json[0]['id']
+    filtered_courses = Course.objects.filter(id=courses[0].id)
+    # assert filtered_courses == res
+
+@pytest.mark.django_db
+def test_
