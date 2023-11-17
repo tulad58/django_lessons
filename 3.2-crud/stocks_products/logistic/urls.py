7095ebb51d04921eb54722dc24fd1c05a1984ee8
Tulad95 <link7800@yandex.ru>
2023-11-17 07:41:39 +0600

    t2

--- 3.2-crud/stocks_products/logistic/urls.py
+++ 3.2-crud/stocks_products/logistic/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from django.urls import include, path
+from django.urls import path
 from logistic.views import ProductViewSet, StockViewSet, sample_view
 
 router = DefaultRouter()
