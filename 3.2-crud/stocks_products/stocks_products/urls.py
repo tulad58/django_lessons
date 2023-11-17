36bb3edff889d817f29cdecba4570be1ba25d6a4
Tulad95 <link7800@yandex.ru>
2023-11-17 07:26:54 +0600

    test urls

--- 3.2-crud/stocks_products/stocks_products/urls.py
+++ 3.2-crud/stocks_products/stocks_products/urls.py
@@ -20,5 +20,4 @@ from logistic.views import sample_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('logistic.urls')),
-    path('api/v1/test/', sample_view)
 ]
