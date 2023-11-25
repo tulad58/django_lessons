8212f6893261d1f601493350b5eec7d6c7207e5b
Tulad95 <link7800@yandex.ru>
2023-11-25 17:23:43 +0600

    2

--- 3.1-drf-intro/smart_home/smart_home/settings.py
+++ 3.1-drf-intro/smart_home/smart_home/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'smart_home.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'smart_home',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-        'PASSWORD': 'admin',
-        'USER': 'postgres'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
